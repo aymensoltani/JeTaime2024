840192a1df7aaf55e0cf5aac48bc562a0ec5f976
aymensoltani <95870641+aymensoltani@users.noreply.github.com>
2024-12-27 21:52:44 +0100

    Update function.js

--- function.js
+++ function.js
@@ -4,7 +4,7 @@ var clientWidth = $(window).width();
 var clientHeight = $(window).height();
 
 $(function () {
-    // setup garden
+    // setup garden1
 	$loveHeart = $("#loveHeart");
 	var offsetX = $loveHeart.width() / 2;
 	var offsetY = $loveHeart.height() / 2 - 55;
